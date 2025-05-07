92de2441b866c6e5a04743fd91c5d24ab55f9d3b
Chandan-Singh-A <singh.chandan1309@gmail.com>
2025-05-07 17:58:01 +0530

    Mobx Store Provider

---
+++ src/context/storeProvider.tsx
@@ -0,0 +1,21 @@
+import { useContext, createContext, } from "react";
+import rootStore, { RootStore } from "../store";
+import { useLocalObservable } from "mobx-react-lite";
+
+const StoreContext = createContext<RootStore | null>(null);
+interface StoreProviderProps {
+    children: React.ReactNode;
+}
+
+export const StoreProvider = ({ children }: StoreProviderProps) => {
+    const store = useLocalObservable(() => rootStore);
+    return<StoreContext.Provider value={store}>{children}</StoreContext.Provider>
+}
+
+export const useStore=()=>{
+    const store=useContext(StoreContext)
+    if(!store){
+        throw new Error("useStore must be used within a StoreProvider")
+    }
+    return store
+}
